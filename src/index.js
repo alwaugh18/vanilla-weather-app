3d0c2b5a8a4a76378b548792971deaa9c4c710d0
alwaugh18 <alwaugh18@gmail.com>
2023-01-08 20:07:05 -0600

    fixed apiUrl

--- src/index.js
+++ src/index.js
@@ -16,11 +16,12 @@ let minutes = now.getMinutes();
 let date = (dateTime.innerHTML = `${day} ${hours}:${minutes} `);
 
 let apiKey = "oedd429506fc7daf6f93a04b3atc1af8";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Paris&key=oedd429506fc7daf6f93a04b3atc1af8&units=metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=query&key=oedd429506fc7daf6f93a04b3atc1af8&units=metric`;
 
 function city(response) {
   console.log(response);
+  let search = document.querySelector("#city");
+  search.innerHTML = response.data.city.value;
 }
 
 axios.get(apiUrl).then(city);
